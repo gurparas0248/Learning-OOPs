5df5826e657a63b91069a0e09f56d8ca1be93fde
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-12-21 01:14:21 +0530

    Types of Constructor

--- OOPs/OOPs2.java
+++ OOPs/OOPs2.java
@@ -1,10 +1,12 @@
 package OOPs;
 
+import java.sql.SQLOutput;
+
 public class OOPs2 {
     public static void main(String[] args) {
-        Student1 s1=new Student1("Paras");
-        System.out.println(s1.name);
-
+        Student1 s1=new Student1();
+        Student1 s2=new Student1("Paras");
+        Student1 s3=new Student1(12);
     }
 
 }
@@ -13,8 +15,18 @@ class Student1{
     String name;
     int roll;
 
+//    non-parameterized constructor
+    Student1(){
+        System.out.println("Constructor is called");
+    }
+
+
+//    parameterized constructor
     Student1(String name){
         this.name=name;
-    }
+   }
+   Student1(int roll){
+        this.roll=roll;
+   }
 
 }
