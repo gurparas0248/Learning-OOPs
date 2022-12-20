4ba401c3934d18b0e1a6c61503b82e8461b7bde7
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-12-21 01:37:14 +0530

    shallow and deep Copy constructors

--- OOPs/OOPs2.java
+++ OOPs/OOPs2.java
@@ -8,9 +8,16 @@ public class OOPs2 {
         s1.name="Paras";
         s1.roll=8;
         s1.pass="abcd";
+        s1.marks[0]=100;
+        s1.marks[1]=90;
+        s1.marks[2]=40;
 
         Student1 s2=new Student1(s1);
         s2.pass="xyz";
+        s1.marks[2]=100;
+        for(int i=0;i<3;i++){
+            System.out.println(s2.marks[i]); 
+        }
     }
 
 }
@@ -19,24 +26,43 @@ class Student1{
     String name;
     int roll;
     String pass;
+    int marks[];
 
-    //copy constructor
+
+    //Deep copy constructor
     Student1(Student1 x){
+        marks=new int[3];
         this.name=x.name;
         this.roll=x.roll;
+        for (int i = 0; i <marks.length; i++) {
+            this.marks[i]=x.marks[i];
+        }
+
     }
+
+
+    //Shallow copy constructor
+//    Student1(Student1 x){
+//        marks=new int[3];
+//        this.name=x.name;
+//        this.roll=x.roll;
+//        this.marks=x.marks;
+//    }
 
 //    non-parameterized constructor
     Student1(){
+        marks=new int[3];
         System.out.println("Constructor is called");
     }
 
 
 //    parameterized constructor
     Student1(String name){
+        marks=new int[3];
         this.name=name;
    }
    Student1(int roll){
+       marks=new int[3];
         this.roll=roll;
    }
 
