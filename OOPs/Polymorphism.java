a014779b0e431e15c1d21f282a42ef64a72885f2
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-12-21 21:47:36 +0530

    Method Overriding

--- OOPs/Polymorphism.java
+++ OOPs/Polymorphism.java
@@ -1,14 +1,29 @@
 package OOPs;
 
+import java.sql.SQLOutput;
+
 public class Polymorphism {
     public static void main(String[] args) {
         Calculator cal=new Calculator();
         System.out.println(cal.sum(1,2));
         System.out.println(cal.sum((float)1.5,(float)2.5));
         System.out.println(cal.sum(1,2,4));
+
+        Deer d=new Deer();
+        d.eat();
     }
 }
 
+class Animals{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+class Deer extends Animals{
+    void eat(){
+        System.out.println("Eats grass");
+    }
+}
 class Calculator{
     int sum(int a,int b){
         return a+b;
