4ec263cc48176f0d4e986ea81615556c298e5d92
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-12-21 21:16:08 +0530

    Hierarchial inheritance

--- OOPs/Ingeritance.java
+++ OOPs/Ingeritance.java
@@ -2,10 +2,7 @@ package OOPs;
 
 public class Ingeritance {
     public static void main(String[] args) {
-        Dog dobby=new Dog();
-        dobby.eat();
-        dobby.legs=4;
-        System.out.println(dobby.legs);
+        
 
     }
 }
@@ -22,13 +19,23 @@ class Animal{
 }
 
 class Mammal extends Animal{
-    int legs;
+    void walk(){
+        System.out.println("walks");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("Swim");
+    }
 }
 
-class Dog extends Mammal{
-    String breed;
-
+class Bird extends Animal{
+    void fly(){
+        System.out.println("fly");
+    }
 }
+
+
 
 //derived class
 //class Fish extends Animal{
